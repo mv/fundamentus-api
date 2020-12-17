e07850e72105b01b323d8a47bfe38eedcb45a760
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2020-12-17 09:49:07 -0300

    New example: Top 10 by Div.Yield.

---
+++ filter.top-10-DivYield.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#
+# Ref:
+#   https://stackoverflow.com/questions/38218501/python-get-top-n-keys-with-value-as-dictionary
+#   https://stackoverflow.com/questions/30250715/how-do-you-get-the-first-3-elements-in-python-ordereddict/30250803
+#   https://stackoverflow.com/questions/21062781/shortest-way-to-get-first-item-of-ordereddict-in-python-3
+#
+#   https://www.geeksforgeeks.org/sorted-function-python/
+#   https://realpython.com/python-sort/
+#
+
+from fundamentus import get_fundamentus
+from fundamentus import print_csv
+
+from collections import OrderedDict
+
+
+if __name__ == '__main__':
+
+    # Parametros usados em 'Busca avancada por empresa'
+    params = {'pl_min'          : '0.001',
+              'divy_min'        : '0',
+              'divy_max'        : '',
+              }
+
+    data = get_fundamentus(params)
+
+    # Top 10:
+    top_10 = OrderedDict(sorted(data.items(), key = lambda x: x[1]['DY'], reverse=True)[:10])
+
+    print_csv(top_10)
+
+
+
+    # Reorder by Div Yield
+#   data = OrderedDict(sorted(data.items(), key = lambda x: x[1]["DY"]))
+#   data = OrderedDict(sorted(data.items(), key = lambda x: x[1]["DY"], reverse=True))
+
+    # Top 10
+#   top_list = list(data)[:10]
+#   top_data = OrderedDict()
+#   for item in top_list: top_data[item] = data[item]
+
+#   print_csv(top_data)
+
+
