97f647b39a87f9f8f675c23edc3af4996c9b1e25
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2020-12-24 15:33:03 -0300

    Top10 using DataFrame sort.

--- filter.top-10-DivYield.py
+++ filter.top-10-DivYield.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 #
-# Ref:
-#   https://stackoverflow.com/questions/38218501/python-get-top-n-keys-with-value-as-dictionary
-#   https://stackoverflow.com/questions/30250715/how-do-you-get-the-first-3-elements-in-python-ordereddict/30250803
-#   https://stackoverflow.com/questions/21062781/shortest-way-to-get-first-item-of-ordereddict-in-python-3
 #
 
 from fundamentus import get_fundamentus
 from fundamentus import print_csv
-
-from collections import OrderedDict
+from fundamentus import print_table
 
 
 if __name__ == '__main__':
@@ -23,16 +18,8 @@ if __name__ == '__main__':
     data = get_fundamentus(params)
 
     # Top 10:
-    top_10 = OrderedDict(sorted(data.items(), key = lambda x: x[1]['DY'], reverse=True)[:10])
+    data.sort_values( by='dy', inplace=True, ascending=False )
+    top_10 = data[:10]
 
     print_csv(top_10)
-
-
-    # Top 10
-#   top_list = list(data)[:10]
-#   top_data = OrderedDict()
-#   for item in top_list: top_data[item] = data[item]
-
-#   print_csv(top_data)
-
 
