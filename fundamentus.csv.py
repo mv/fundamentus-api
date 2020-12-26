00e75054be3eeda643bc5f86540332df854ae1bc
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2020-12-26 16:57:47 -0300

    Filter by pandas/DataFrame.

--- fundamentus.csv.py
+++ fundamentus.csv.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
 #
+#
 
 from fundamentus import get_resultado
 from fundamentus import print_csv
 
-
 if __name__ == '__main__':
 
-    # Parametros usados em 'Busca avancada por empresa'
-    params = {'pl_min'  : '',
-              'pl_max'  : '',
-              'roic_min': '',
-              'roic_max': '',
-              'roe_min' : '',
-              'roe_max' : '',
-              }
-
-#   data = get_resultado(params)
     data = get_resultado()
 
     # Reorder by ticker
-    print_csv( data.sort_values(by='Papel') )
+    data = data.sort_index(ascending=True)
 
+    # filter on DataFrame
+    data = data[ data.pl   > 0   ]
+    data = data[ data.pl   < 100 ]
+    data = data[ data.roe  > 0   ]
+    data = data[ data.roic > 0   ]
+
+    print_csv(data)
 
