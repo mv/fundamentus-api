c2baa9284d86b123eed36521cbefb007864fba31
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2020-12-26 18:44:10 -0300

    Add info.

--- fundamentus/__init__.py
+++ fundamentus/__init__.py
@@ -1,5 +1,16 @@
 
+"""
+Fundamentus:
+  Info:
+    Get data from http://fundamentus.com.br/ into Pandas.
+
+  Ref:
+    https://github.com/mv/fundamentus
+"""
+
+
 __all__ = ['get_resultado','get_resultado_raw','print_csv','print_table']
+
 
 from fundamentus.resultado  import get_resultado
 from fundamentus.resultado  import get_resultado_raw
@@ -8,13 +19,11 @@ from fundamentus.detalhes   import get_detalhes
 from fundamentus.detalhes   import get_detalhes_raw
 from fundamentus.detalhes   import get_detalhes_list
 
-from fundamentus.setor       import get_setor_data
-from fundamentus.setor       import get_setor_id
-from fundamentus.setor       import list_setor
+from fundamentus.setor      import get_setor_data
+from fundamentus.setor      import get_setor_id
+from fundamentus.setor      import list_setor
 
-from fundamentus.utils       import print_csv
-from fundamentus.utils       import print_table
+from fundamentus.utils      import print_csv
+from fundamentus.utils      import print_table
 
-
-#rom fundamentus.buscaavancada import get_resultado_raw
 
