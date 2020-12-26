d597b3d9630d685c5250dac1dd72ef48325d2eeb
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2020-12-26 16:08:31 -0300

    Reorder.

--- fundamentus/__init__.py
+++ fundamentus/__init__.py
@@ -4,14 +4,14 @@ __all__ = ['get_fundamentus','get_fundamentus_raw','print_csv','print_table']
 from fundamentus.fundamentus import get_fundamentus
 from fundamentus.fundamentus import get_fundamentus_raw
 
+from fundamentus.details     import get_details
+from fundamentus.details     import get_details_list
+from fundamentus.details     import get_details_raw
 
 from fundamentus.setor       import get_setor_data
 from fundamentus.setor       import get_setor_id
 from fundamentus.setor       import list_setor
 
-from fundamentus.details     import get_details
-from fundamentus.details     import get_details_list
-from fundamentus.details     import get_details_raw
 from fundamentus.utils       import print_csv
 from fundamentus.utils       import print_table
 
