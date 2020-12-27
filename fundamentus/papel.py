0e5718189698672db611d9014afc26fd3734c02c
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2020-12-27 18:04:17 -0300

    New: 'papel' from a faster page.

---
+++ fundamentus/papel.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+"""
+Papel:
+  Info results when acessing 'http://fundamentus.com.br/detalhes.php' with
+  no parameters
+"""
+
+import requests
+import requests_cache
+import pandas   as pd
+
+
+def get_list_papel():
+    """
+    Papel: List of names only.
+
+    Output:
+      list
+    """
+    df  = get_df_papel()
+    lst = list(df['Papel'])
+
+    return lst
+
+
+def get_df_papel():
+    """
+    Papel: Get list of names and descriptions
+      URL:
+        http://fundamentus.com.br/detalhes.php
+
+    Output:
+      df
+    """
+
+    url = 'http://fundamentus.com.br/detalhes.php'
+    hdr = {'User-agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201',
+           'Accept': 'text/html, text/plain, text/css, text/sgml, */*;q=0.01',
+           'Accept-Encoding': 'gzip, deflate',
+           }
+
+    with requests_cache.enabled():
+        content = requests.get(url, headers=hdr)
+
+        if content.from_cache:
+            pass
+        else:
+            time.sleep(.500) # 500 ms
+
+
+    ## parse
+    df = pd.read_html(content.text)[0]
+
+    return df
+
+
