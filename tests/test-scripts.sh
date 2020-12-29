44bc5fcee10253da00b5fbf13b422b191284db40
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2020-12-29 18:03:22 -0300

    Show each script execution.

--- tests/test-scripts.sh
+++ tests/test-scripts.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-python3 bin/detail.all.py               > /dev/null
-python3 bin/detail.my-list.py           > /dev/null
-python3 bin/detail.show.py              > /dev/null
-python3 bin/filter.example.py           > /dev/null
-python3 bin/filter.finan.csv.py         > /dev/null
-python3 bin/filter.finan.table.py       > /dev/null
-python3 bin/filter.top-10-DivYield.py   > /dev/null
-python3 bin/fundamentus.csv.py          > /dev/null
-python3 bin/magic_formula.simple.py     > /dev/null
+echodo() {
+    echo "###"
+    echo "### $@"
+    echo "###"
+    "$@" > /dev/null
+}
+
+
+echodo python3 bin/detail.all.py
+echodo python3 bin/detail.my-list.py
+echodo python3 bin/detail.show.py
+echodo python3 bin/filter.example.py
+echodo python3 bin/filter.finan.csv.py
+echodo python3 bin/filter.finan.table.py
+echodo python3 bin/filter.top-10-DivYield.py
+echodo python3 bin/fundamentus.csv.py
+echodo python3 bin/magic_formula.simple.py
 
