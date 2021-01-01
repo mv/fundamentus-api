04b6a5ede6878eb0cb2cb6a94224a1cbf6d8558a
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2021-01-01 12:58:51 -0300

    pytest.parametrize.

--- tests/test_setor.py
+++ tests/test_setor.py
@@ -2,18 +2,33 @@
 from fundamentus import setor
 
 import pandas as pd
+import pytest
 
 
-def test_list_papel_setor():
-    lst = setor.list_papel_setor(35)
+###
+@pytest.mark.parametrize(
+    'param',[ pytest.param(35, id='fin' )
+            , pytest.param(38, id='seg' )
+            , pytest.param(38, id='prev')
+            ])
+def test_list_papel_setor__len(param):
+
+    lst = setor.list_papel_setor(param)
     assert len(lst) > 0
 
 
-def test_get_setor_id():
-    assert setor.get_setor_id('financeiro' ) == 35
-    assert setor.get_setor_id('previdencia') == 38
-    assert setor.get_setor_id('seguros'    ) == 38
+###
+@pytest.mark.parametrize(
+    'param',[ pytest.param( ['financeiro' , 35], id='fin' )
+            , pytest.param( ['seguros'    , 38], id='seg' )
+            , pytest.param( ['previdencia', 38], id='prev')
+            ])
+def test_get_setor_id__setor(param):
 
+    assert setor.get_setor_id(param[0]) == param[1]
+
+
+###
 def test_print_setores(capfd):
     setor.print_setores()
     out, err = capfd.readouterr()
