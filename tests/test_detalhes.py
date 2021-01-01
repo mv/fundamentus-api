b216f7150521eb27165aaa11b2491a8b008b63be
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2021-01-01 12:58:23 -0300

    pytest.parametrize: 'papel' in list.

--- tests/test_detalhes.py
+++ tests/test_detalhes.py
@@ -4,6 +4,8 @@ from fundamentus import detalhes
 import pytest
 import pandas as pd
 
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 def test_get_detalhes_raw():
@@ -88,16 +90,21 @@ def test_get_papel__as_list():
     assert df.index[0]   == 'ITSA4'
     assert df.index[1]   == 'WEGE3'
 
+
+###
 @pytest.mark.parametrize('papel',['ABEV3','ITSA4','WEGE3',])
 def test_get_papel__as_papel(papel):
     df = detalhes.get_papel(papel)
     assert df['Papel'][0] == papel
 
 
+###
 def test_list_papel_all__len():
     lst = detalhes.list_papel_all()
     assert len(lst) > 5
 
+
+###
 @pytest.mark.parametrize('papel',['ABEV3','ITSA4','WEGE3'])
 def test_list_papel_all__in(papel):
     lst = detalhes.list_papel_all()
