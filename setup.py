f1916c455153acfcba8db43ef979666ed654a2a7
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2021-01-06 17:03:59 -0300

    Using 'tbump'.

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name = 'fundamentus',
-    packages = find_packages(where='src'),
-    package_dir = {'': 'src'},
+    name='fundamentus',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
 
-    version='0.0.0.0',
+    version='0.0.0',
 
     scripts=[
         'bin/fundamentus.csv.py',
